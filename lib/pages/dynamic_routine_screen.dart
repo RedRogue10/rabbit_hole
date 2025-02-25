07ae42e4ae4a0635004c149661d2fa59cbcc7245
jrolana <olanajhoanna@gmail.com>
2025-02-26 00:49:09 +0800

    Merged with main

--- lib/pages/dynamic_routine_screen.dart
+++ lib/pages/dynamic_routine_screen.dart
@@ -15,75 +15,73 @@ class DynamicHomeRoutine extends StatefulWidget {
 class _DynamicHomeRoutineState extends State<DynamicHomeRoutine> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Container(
-            margin: const EdgeInsets.only(bottom: 70),
-            child: Column(
-              children: [
-                Column(
-                  children: [
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text("All Routines",
-                          style: GoogleFonts.lexend(
-                              textStyle: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                          ))),
-                    ),
-                    const AllRoutines(),
-                  ],
-                ),
-                //Completed Today
-                Column(
-                  children: [
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text("Completed Today",
-                          style: GoogleFonts.lexend(
-                              textStyle: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                          ))),
-                    ),
-                    const CompleteTodayBlock(),
-                  ],
-                ),
-                //Upcoming Today
-                // Column(
-                //   children: [
-                //     Align(
-                //       alignment: Alignment.centerLeft,
-                //       child: Text("Upcoming Today",
-                //           style: GoogleFonts.lexend(
-                //               textStyle: const TextStyle(
-                //             fontWeight: FontWeight.bold,
-                //             fontSize: 18,
-                //           ))),
-                //     ),
-                //     const UpcomingTodayBlock(),
-                //   ],
-                // ),
-                //Other Routines
-                Column(
-                  children: [
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text("Other Routines",
-                          style: GoogleFonts.lexend(
-                              textStyle: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                          ))),
-                    ),
-                    const AllRoutines(),
-                  ],
-                ),
-              ],
-            ),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Container(
+          margin: const EdgeInsets.only(bottom: 70),
+          child: Column(
+            children: [
+              Column(
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text("All Routines",
+                        style: GoogleFonts.lexend(
+                            textStyle: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ))),
+                  ),
+                  const AllRoutines(),
+                ],
+              ),
+              //Completed Today
+              Column(
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text("Completed Today",
+                        style: GoogleFonts.lexend(
+                            textStyle: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ))),
+                  ),
+                  const CompleteTodayBlock(),
+                ],
+              ),
+              //Upcoming Today
+              // Column(
+              //   children: [
+              //     Align(
+              //       alignment: Alignment.centerLeft,
+              //       child: Text("Upcoming Today",
+              //           style: GoogleFonts.lexend(
+              //               textStyle: const TextStyle(
+              //             fontWeight: FontWeight.bold,
+              //             fontSize: 18,
+              //           ))),
+              //     ),
+              //     const UpcomingTodayBlock(),
+              //   ],
+              // ),
+              //Other Routines
+              Column(
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text("Other Routines",
+                        style: GoogleFonts.lexend(
+                            textStyle: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ))),
+                  ),
+                  const AllRoutines(),
+                ],
+              ),
+            ],
           ),
         ),
       ),
--- lib/pages/routine_creation.dart
+++ lib/pages/routine_creation.dart
@@ -69,102 +69,100 @@ class _RoutineCreationDefaultState extends State<RoutineCreation>
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
-    return MaterialApp(
-      home: Scaffold(
-        floatingActionButton: Container(
-          margin: EdgeInsets.only(bottom: 16),
-          child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Row(
-                  children: [
-                    IconButton(
-                        onPressed: () {
-                          addActivity();
-                        },
-                        style: TextButton.styleFrom(
-                            backgroundColor: StyleColor.primary),
-                        icon: Icon(
-                          Icons.playlist_add_rounded,
-                          color: Colors.white,
-                        )),
-                    ElevatedButton(
-                        onPressed: () {
-                          addTaskBlock();
-                        },
-                        child: Text('Add Task Block')),
-                  ],
-                ),
-                SizedBox(height: 10),
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: StyleColor.primary),
-                    onPressed: () {
-                      if( routineName == "Click to set routine name"){
-                        setNameDialog();
-                      }else{
-                      createRoutineDialog();}
-                    },
-                    child: Text(
-                      "Create Routine",
-                      style: TextStyle(color: Colors.white),
-                    )),
-              ],
-            ),
-          ]),
-        ),
-        appBar: AppBar(
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back),
-            tooltip: 'Home',
-            onPressed: () {
-              Navigator.pop(context);
-            },
+    return Scaffold(
+      floatingActionButton: Container(
+        margin: EdgeInsets.only(bottom: 16),
+        child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        addActivity();
+                      },
+                      style: TextButton.styleFrom(
+                          backgroundColor: StyleColor.primary),
+                      icon: Icon(
+                        Icons.playlist_add_rounded,
+                        color: Colors.white,
+                      )),
+                  ElevatedButton(
+                      onPressed: () {
+                        addTaskBlock();
+                      },
+                      child: Text('Add Task Block')),
+                ],
+              ),
+              SizedBox(height: 10),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: StyleColor.primary),
+                  onPressed: () {
+                    if (routineName == "Click to set routine name") {
+                      setNameDialog();
+                    } else {
+                      createRoutineDialog();
+                    }
+                  },
+                  child: Text(
+                    "Create Routine",
+                    style: TextStyle(color: Colors.white),
+                  )),
+            ],
           ),
-          title: const Text('Create a Routine'),
+        ]),
+      ),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          tooltip: 'Home',
+          onPressed: () {
+            Navigator.pop(context);
+          },
         ),
-        body: Column(
-          children: [
-            titleText(),
-            SizedBox(height: 20),
-            Expanded(
-                child: ListView.separated(
-                    itemBuilder: (context, index) {
-                      var block = activityBlocks[index];
-                      return ListTile(
-                        title: block,
-                        trailing: IconButton(
-                            onPressed: () {
-                              setState(() {
-                                if (block is TaskBlock) {
-                                  String? category =
-                                      block.getSelectedCategory();
+        title: const Text('Create a Routine'),
+      ),
+      body: Column(
+        children: [
+          titleText(),
+          SizedBox(height: 20),
+          Expanded(
+              child: ListView.separated(
+                  itemBuilder: (context, index) {
+                    var block = activityBlocks[index];
+                    return ListTile(
+                      title: block,
+                      trailing: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              if (block is TaskBlock) {
+                                String? category = block.getSelectedCategory();
 
-                                  remainingCategories.add(category);
-                                  selectedCategories.remove(category);
-                                  activityBlocks.removeAt(index);
-                                  actCount -= 1;
-                                } else {
-                                  activityBlocks.removeAt(index);
-                                  actCount -= 1;
-                                }
-                              });
-                            },
-                            icon: Icon(
-                              Icons.delete,
-                              size: width * 0.06,
-                            )),
-                      );
-                    },
-                    separatorBuilder: (context, index) {
-                      return SizedBox(
-                        height: height * 0.0001,
-                      );
-                    },
-                    itemCount: actCount))
-          ],
-        ),
+                                remainingCategories.add(category);
+                                selectedCategories.remove(category);
+                                activityBlocks.removeAt(index);
+                                actCount -= 1;
+                              } else {
+                                activityBlocks.removeAt(index);
+                                actCount -= 1;
+                              }
+                            });
+                          },
+                          icon: Icon(
+                            Icons.delete,
+                            size: width * 0.06,
+                          )),
+                    );
+                  },
+                  separatorBuilder: (context, index) {
+                    return SizedBox(
+                      height: height * 0.0001,
+                    );
+                  },
+                  itemCount: actCount))
+        ],
       ),
     );
   }
@@ -248,7 +246,7 @@ class _RoutineCreationDefaultState extends State<RoutineCreation>
             TextButton(
               child: Text('SUBMIT'),
               onPressed: () {
-                if (inputController.text.isEmpty){
+                if (inputController.text.isEmpty) {
                   inputController.text = "Click to set routine name";
                 }
                 setState(() {
@@ -261,30 +259,30 @@ class _RoutineCreationDefaultState extends State<RoutineCreation>
         ),
       );
   Future setNameDialog() => showDialog(
-    context: context,
-    builder: (context) => AlertDialog(
-      title: Text('Enter Routine Name First'),
-      content: TextField(
-        controller: inputController,
-        autofocus: true,
-        decoration: InputDecoration(hintText: "Enter routine name"),
-      ),
-      actions: [
-        TextButton(
-          child: Text('SUBMIT'),
-          onPressed: () {
-            if (inputController.text.isEmpty){
-              inputController.text = "Click to set routine name";
-            }
-            setState(() {
-              Navigator.pop(context);
-              routineName = inputController.text;
-            });
-          },
-        )
-      ],
-    ),
-  );
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Enter Routine Name First'),
+          content: TextField(
+            controller: inputController,
+            autofocus: true,
+            decoration: InputDecoration(hintText: "Enter routine name"),
+          ),
+          actions: [
+            TextButton(
+              child: Text('SUBMIT'),
+              onPressed: () {
+                if (inputController.text.isEmpty) {
+                  inputController.text = "Click to set routine name";
+                }
+                setState(() {
+                  Navigator.pop(context);
+                  routineName = inputController.text;
+                });
+              },
+            )
+          ],
+        ),
+      );
   final List<DayInWeek> _days = [
     DayInWeek("Mo", dayKey: "monday"),
     DayInWeek("Tu", dayKey: "tuesday"),
@@ -347,7 +345,7 @@ class _RoutineCreationDefaultState extends State<RoutineCreation>
         color: 128390830,
         name: routineName,
         numActivities: actCount,
-        repeatDaysCount: repeatDays.isEmpty? 1:repeatDays.length.toDouble(),
+        repeatDaysCount: repeatDays.isEmpty ? 1 : repeatDays.length.toDouble(),
         repeatWeeksCount: 1,
         daysOfWeek: repeatDays);
     List activities = [];
@@ -380,7 +378,4 @@ class _RoutineCreationDefaultState extends State<RoutineCreation>
               builder: (context) => RoutineSessionLanding(result)));
     });
   }
-
-
-
 }
